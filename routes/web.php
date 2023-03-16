5ff5126d1379dd076bd7ee62cdf6167c68b9fd33
alynnesantos <alynnebianca774@gmail.com>
2023-03-16 13:49:35 -0300

    criação de novas rotas e resgate de registro

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create']);
+Route::get('/events/{id}', [EventController::class, 'show']);
+Route::post('/events', [EventController::class, 'store']);
 
 Route::get('/contact', function () {
     return view('contact');
-});
-
-Route::get('/pro', function () {
-    return view('products');
 });
