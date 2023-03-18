88e1f32b35d57a322dfd85cc28df8fd087aedea8
alynnesantos <111379595+alynnesantos@users.noreply.github.com>
2023-03-18 00:34:18 -0300

    Adicionando items na tabela

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -28,7 +28,21 @@
       </div>
       <div class="form-group">
         <label for="title">Descrição:</label>
-        <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?" required></textarea>
+        <input type="text"class="form-control" id="description" name="description" placeholder="Descrição" required></input>
+      <div class="form-group">
+        <label for="title"> Adicione itens de infraestrutura: </label>
+        <div class="form-group">
+          <input type="checkbox" name="items[]" value="Cadeiras"> Cadeiras
+        </div>
+        <div class="form-group">
+          <input type="checkbox" name="items[]" value="Palco"> Palco
+        </div>
+        <div class="form-group">
+          <input type="checkbox" name="items[]" value="Open food"> Open food
+        </div>
+        <div class="form-group">
+          <input type="checkbox" name="items[]" value="Brindes"> Brindes
+        </div>
       </div>
       <input type="submit" class="btn btn-primary" value="Criar Evento">
    </form>
