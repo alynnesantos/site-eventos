c0e007123e4a50b1a90f44b3e73c3b4998465e2d
alynnesantos <alynnebianca774@gmail.com>
2023-04-14 13:35:43 -0300

    Removendo presença do evento

--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -60,7 +60,14 @@
                     <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
                     <td>{{ count($event->users) }}</td>
                     <td>
-                        <a href="#">Sair do evento</a>
+                    <form action="/events/leave/{{ $event->id }}" method="POST">
+                        @csrf
+                        @method("DELETE")
+                        <button type="submit" class-="btn btn-danger delete-btn"> 
+                            <ion-icon name="trash-outline"></ion-icon>
+                            Sair do evento       
+                        </button>
+                    </form>
                     </td>
                 </tr>
             @endforeach
@@ -68,7 +75,6 @@
     </table>
 @else
 <p>Você ainda não está participando de nenhum evento, <a href="/"> veja todos os eventos</a></p>
-
 @endif
 </div>
 
